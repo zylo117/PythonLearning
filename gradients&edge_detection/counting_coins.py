252230b6deb5fd999c1bdf8d149bf87c8bc5d40b
zylo117 <zylo117@hotmail.com>
2017-10-16 17:00:44 +0800

    Comment added

--- gradients&edge_detection/counting_coins.py
+++ gradients&edge_detection/counting_coins.py
@@ -27,6 +27,24 @@ cv2.imshow("Theshold", th_image)
 
 edged = auto_canny.auto_canny(th_image)
 cv2.imshow("Edged", edged)
+
+# contours: : 检测到的轮廓，每个轮廓存储为一个点向量，即用point类型的vector，例如可为类型vector<vector<Point> >。
+# hierarchy: 可选的输出向量，包含图像的拓扑信息。 每个轮廓contours[i],
+# hierarchy[i][0] , 后一个轮廓，
+# hierarchy[i][1] , 前一个轮廓，
+# hierarchy[i][2] , 父轮廓，
+# hierarchy[i][3], 内嵌轮廓的索引编号。
+# 如果没有对应项，hierarchy[i]中的对应项设为负数。
+# mode: 检索模式，可选模式包括
+# RETR_EXTERNAL: 只监测最外层轮扩。hierarchy[i][2] ＝ hierarchy[i][3] ＝ －1
+# RETR_LIST: 提取所有轮廓，并放置在list中。检测的轮廓不建立等级关系。
+# RETR_CCOMP: 提取所有轮廓，并将其组织为双层结构，顶层为联通域的外围边界，次层为空的内层边界。
+# RETR_TREE: 提取所有轮廓，并重新建立网状的轮廓结构。
+# method: 轮廓的近似办法，包括
+# CHAIN_APPROX_NONE: 获取每个轮廓的每个像素，相邻两点像素位置差不超过1，max(abs(x1-x2),abs(y1-y2)) == 1
+# CHAIN_APPROX_SIMPLE: 压缩水平方向，垂直方向，对角线方向的元素，只保留该方向的终点坐标
+# CHAIN_APPROX_TC89_Lland T. Chin. On the detection of dominant points on digital curves. Pattern Analysis and Machine Intelligence, IEEE Transactions on, 11(8):859–872, 1989.
+# offSet: 每个轮廓点的可选偏移量，默认Point(), 当ROI图像中找出的轮廓需要在整个图中进行分析时，可利用这个参数。
 (_, cnts, _) = cv2.findContours(th_image.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
 print("I count {} coins in this image".format(len(cnts)))
